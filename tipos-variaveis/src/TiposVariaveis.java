7861837db8aa041da15d92a5a5d1ce9d257297ef
Hildélio Júnior <hildelio@gmail.com>
2023-02-26 22:31:11 -0300

    Estudo de tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.00;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 5;
+        numero = 10;
+        final double PI = 3.14;
+        System.out.println(numero * salarioMinimo / numeroCurto2 + PI);
     }
 }
